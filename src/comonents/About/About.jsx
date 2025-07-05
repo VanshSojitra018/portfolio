e37bfd01ae2bb236bb10059d8bc30cb5ff1caa17
Vansh Sojitra <vanshsojitra1818@gmail.com>
2025-07-05 10:24:55 +0530

    second commit

--- src/comonents/About/About.jsx
+++ src/comonents/About/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1twmuKnJ0Uh_clzKmGzyzq2S86npGxFNQ/view?usp=sharing"
+            href="https://drive.google.com/file/d/1DgQvN80V0koauC2UXlQEcMIyLWG4Yz_o/view?usp=drive_link"
             target="_blank"
             rel="nooper noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
